134ce87bda57044efba92cfb33b36db44c7e1bc5
KSean222 <44050761+KSean222@users.noreply.github.com>
2019-11-30 22:53:01 +1000

    Update MessageCreatedListeners.java

--- src/main/java/io/mindustry/plugin/MessageCreatedListeners.java
+++ src/main/java/io/mindustry/plugin/MessageCreatedListeners.java
@@ -12,12 +12,15 @@ import io.anuke.mindustry.content.Blocks;
 import io.anuke.mindustry.core.GameState;
 import io.anuke.mindustry.core.World;
 import io.anuke.mindustry.game.Gamemode;
+import io.anuke.mindustry.game.Teams;
 import io.anuke.mindustry.io.MapIO;
 import io.anuke.mindustry.io.SaveIO;
 import io.anuke.mindustry.maps.Map;
 import io.anuke.mindustry.world.Tile;
 import io.anuke.mindustry.world.blocks.Floor;
 import io.anuke.mindustry.world.blocks.StaticWall;
+import io.anuke.mindustry.world.blocks.storage.CoreBlock;
+import io.anuke.mindustry.world.meta.BlockFlag;
 import io.mindustry.plugin.discordcommands.DiscordCommands;
 import io.mindustry.plugin.discordcommands.MessageCreatedListener;
 import org.javacord.api.entity.message.MessageAttachment;
@@ -25,6 +28,8 @@ import org.javacord.api.event.message.MessageCreateEvent;
 import org.json.JSONObject;
 
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.zip.InflaterInputStream;
 
 public class MessageCreatedListeners {
@@ -41,6 +46,7 @@ public class MessageCreatedListeners {
             private final String mapHasInvalidName = "This map has an invalid name";
             private final String mapIsInvalid = "This map is invalid";
             private final String mapIsValid = "This map is valid! Please wait for us to review it.";
+            private final String mapInvalidForMode = "This map is invalid for the mode %s";
             private final String invalidMapReply = "%s! Please check pinned messages.";
             private final MapReader mapReader = new MapReader();
             private final String[] validModes = new String[] {
@@ -66,8 +72,9 @@ public class MessageCreatedListeners {
                                             try {
                                                 String reply;
                                                 if(SaveIO.isSaveValid(new DataInputStream(new InflaterInputStream(new ByteArrayInputStream(data))))){
-                                                    MapReader.Map map = mapReader.parseMap(new ByteArrayInputStream(data));
-                                                    reply = valiadateMap(map);
+                                                    Map map = MapIO.createMap(new VirtualFileHandle(data), true);
+                                                    MapReader.Mtile[][] tiles = mapReader.parseMap(new ByteArrayInputStream(data)).tiles;
+                                                    reply = valiadateMap(map, tiles, prefix);
                                                 } else {
                                                     reply = mapIsInvalid;
                                                 }
@@ -96,12 +103,15 @@ public class MessageCreatedListeners {
                     Log.err("JSON data didn't contain map submissions settings!");
                 }
             }
-            private String valiadateMap(MapReader.Map map){
-                if(map.tiles.length == 0) return mapIsInvalid;
-                int mapSize = map.tiles.length * map.tiles[0].length;
+            private String valiadateMap(Map map, MapReader.Mtile[][] tiles, String mode){
+                if(mode.){
+
+                }
+                if(tiles.length == 0) return mapIsInvalid;
+                int mapSize = tiles.length * tiles[0].length;
                 if(mapSize < settings.getInt("minimumMapArea")) return mapTooSmall;
                 int buildableTiles = 0;
-                for(MapReader.Mtile[] row: map.tiles){
+                for(MapReader.Mtile[] row: tiles){
                     for(MapReader.Mtile tile: row){
                         if(tile.wall == Blocks.air){
                             Floor floor = ((Floor)tile.floor);
